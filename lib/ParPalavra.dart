6def0ab000688c44ada029c8432fd0faea07d556
Joel Fausto <joelfausto57@gmail.com>
2022-12-15 15:15:21 -0300

    new

---
+++ lib/ParPalavra.dart
@@ -0,0 +1,28 @@
+import 'package:english_words/english_words.dart';
+
+class ParPalavra {
+  String firstWord = '';
+  String secondWord = '';
+
+  ParPalavra(this.firstWord, this.secondWord);
+
+// Factory -> criar novas palavras usando a base Word Pair
+  factory ParPalavra.constructor() {
+    WordPair word = generateWordPairs().first;
+    ParPalavra p = ParPalavra(word.first, word.second);
+    return p;
+  }
+
+// Function -> Transforma a segunda parte de uma palavra em LowerCase
+  String lowerCase(String word) {
+    return word.toLowerCase();
+  }
+
+// Function -> Muda as palavras para PascalCase
+  String CreateAsPascalCase() {
+    return "${firstWord[0].toUpperCase() + lowerCase(firstWord.substring(1))}${secondWord[0].toUpperCase() + lowerCase(secondWord.substring(1))}";
+  }
+
+// Var que recebe o PascalCase da function
+  late final asPascalCase = CreateAsPascalCase();
+}
